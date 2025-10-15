0a05a062cd89df85eea4b89a43f7c339a3e7d02b
Yasha <yasuhiro.asaka@grauwoelfchen.net>
2025-10-15 19:33:44 +0900

    Add serde_json
    Signed-off-by: Yasha <yasuhiro.asaka@grauwoelfchen.net>

--- src/queue.rs
+++ src/queue.rs
@@ -1,5 +1,6 @@
 use redis::{Commands, Connection, ErrorKind, RedisError, Value};
 use serde::{de::DeserializeOwned, Serialize};
+use serde_json;
 
 pub struct Queue<'c> {
     conn: &'c mut Connection,
